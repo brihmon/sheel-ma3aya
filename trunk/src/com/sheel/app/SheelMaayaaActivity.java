559746e0c2936736f0f0a3b7f6640bd748c49270
hossam.amer12@gmail.com <hossam.amer12@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-11-04 00:21:00 +0000

    Adding the new project folder

---
+++ trunk/src/com/sheel/app/SheelMaayaaActivity.java
@@ -0,0 +1,13 @@
+package com.sheel.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SheelMaayaaActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
