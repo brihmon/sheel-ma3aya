e38d88ae638a5124cad345c680d7ac68cbbdd5ad
hossam.amer12 <hossam.amer12@gmail.com>
2011-11-05 19:07:47 +0000

    Just finished the call and send button actions. Added the permissions in the android.manifest file. Created a simple view for those buttons, and test case to navigate from one view to another. Hence, simulating what will happen during the real application.

---
+++ src/com/sheel/app/PhoneCommunication.java
@@ -0,0 +1,71 @@
+package com.sheel.app;
+
+import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+
+// Sending mail: http://mobile.tutsplus.com/tutorials/android/android-email-intent/
+
+public class PhoneCommunication extends Activity {
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.communicate);
+	}
+
+	public void onClick_call (View v) 
+	 {
+		try {
+	 
+			Intent callIntent = new Intent(Intent.ACTION_DIAL);
+			/*
+			 * User x = new User()
+			 * x.getInfo();
+			 * String x = x.getinfo()
+			 * tel: x;
+			 */
+			
+			callIntent.setData(Uri.parse("tel: 0101577990"));
+			startActivity(callIntent);
+		} catch (ActivityNotFoundException e) {
+			
+			Log.e("PhoneCommunication.java", "Call failed!", e);
+		} 
+		
+	}// end onClick_call
+	
+	public void onClick_send_SMS (View v) 
+	 {
+		try {
+	 
+			/*
+			 * User x = new User()
+			 * x.getInfo();
+			 * String x = x.getinfo()
+			 * tel: x;
+			 */
+						
+			String number = "5556";
+			Intent sendIntent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null));
+			sendIntent.putExtra("sms_body", "Content of the SMS goes here...");
+			startActivity(sendIntent);
+			
+			
+		} catch (ActivityNotFoundException e) {
+			
+			Log.e("PhoneCommunication.java", "SMS failed!", e);
+		} 
+		
+	}// end onClick_call
+
+	
+	
+	
+}
--- src/com/sheel/app/SheelMaayaaActivity.java
+++ src/com/sheel/app/SheelMaayaaActivity.java
@@ -1,7 +1,11 @@
 package com.sheel.app;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 public class SheelMaayaaActivity extends Activity {
     /** Called when the activity is first created. */
@@ -10,4 +14,12 @@ public class SheelMaayaaActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+    
+    
+	 public void onClick_go (View v) 
+	 {
+		 Toast.makeText(getApplicationContext(), "Displaying contact info", Toast.LENGTH_SHORT).show();
+		 startActivity(new Intent(this, PhoneCommunication.class));
+	}	 
+	
 }
