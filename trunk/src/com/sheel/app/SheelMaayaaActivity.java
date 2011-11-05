54e4ae1e056074f26a38bf3fa0c07c9bc96697d2
hossam.amer12@gmail.com <hossam.amer12@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-11-05 19:08:37 +0000

    Removed useless imports.

--- trunk/src/com/sheel/app/SheelMaayaaActivity.java
+++ trunk/src/com/sheel/app/SheelMaayaaActivity.java
@@ -3,7 +3,6 @@ package com.sheel.app;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
 
