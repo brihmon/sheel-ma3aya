d5529ff6375c9abbfc691d4ed502932a05ce13a3
hossam.amer12 <hossam.amer12@gmail.com>
2011-11-11 17:01:43 +0000

    Updated the SMS template of user1.

--- src/com/sheel/app/PhoneCommunication.java
+++ src/com/sheel/app/PhoneCommunication.java
@@ -51,8 +51,26 @@ public class PhoneCommunication extends Activity {
 			 */
 						
 			String number = "5556";
+			String sms_content1 = "Hello User2,";
+			String sms_content2 = "I have seen your offer on Sheel M3aaya app that you " +
+					"have an extra space (N Kilograms) in your bags." +
+					" So, I would like to inform you that " +
+					"I am interested in putting some of my stuff in your bags.";
+			String sms_content3 = "Please contact me at this number if your space is still available.";
+			String sms_content4 = "Thanks in advance :)";
+			String sms_content5 = "Wish you a nice flight.";
+			String sms_content6 = "Best Regards,\nUser1";
+			
+			String sms_content = sms_content1 
+								+ "\n" + "\n" + sms_content2
+								+ "\n" + "\n" + sms_content3
+								+ "\n" + "\n" + sms_content4
+								+ "\n" + "\n" + sms_content5
+								+ "\n" + "\n" + sms_content6;
+			
 			Intent sendIntent = new Intent(Intent.ACTION_VIEW, Uri.fromParts("sms", number, null));
-			sendIntent.putExtra("sms_body", "Content of the SMS goes here...");
+//			sendIntent.putExtra("sms_body", "Content of the SMS goes here...");
+			sendIntent.putExtra("sms_body", sms_content);
 			startActivity(sendIntent);
 			
 			
