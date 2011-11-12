b8b8427c1ef37c0ac9c66b1b208e4bcf153fcefb
amohsen1989@gmail.com <amohsen1989@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-11-12 21:57:12 +0000

    Committing HTTP client

---
+++ trunk/src/com/sheel/app/SheelMaaayaClient.java
@@ -0,0 +1,67 @@
+package com.sheel.app;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+import android.os.Looper;
+import android.util.Log;
+import android.widget.Toast;
+
+
+//Responsible for sending HTTP requests and getting HTTP response
+public abstract class SheelMaaayaClient {
+	
+	 final String SERVER  = "http://sheelmaaayaa.appspot.com";
+     String PATH   = "";
+     HttpResponse RESPONSE = null;
+     Thread THREAD = null;
+     String rspStr = "";
+	
+	public SheelMaaayaClient(){
+		
+	}
+	
+	
+    
+    public void runHttpRequest(String path){
+	   
+	        PATH = path;
+		    rspStr = "";
+		   
+	        THREAD = new Thread(new Runnable()
+	    
+	    	{
+	        @Override
+	        public void run()
+	        {
+	            try
+	            {
+	                 DefaultHttpClient CLIENT = new DefaultHttpClient();   
+	                 RESPONSE = CLIENT.execute(new HttpGet(SERVER + "/test"));
+	                
+		             if (RESPONSE.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
+		                {
+		                 rspStr = EntityUtils.toString(RESPONSE.getEntity());	
+		                }
+	                
+	            }
+	            catch (Exception e)
+	            {
+	            	 rspStr = e.toString();	
+	            }
+	            doSomething();
+	        }
+	    });
+	    
+	    THREAD.start();
+    }
+    
+    //To be implemented by according to the activity needs
+    public abstract void doSomething();
+    	
+}
+
