3dfe4ad96803874053727ed050f1a4229a6f82e4
amohsen1989 <amohsen1989@gmail.com>
2011-11-15 16:37:59 +0000

    Commiting insert offer activity (not finalized)

---
+++ src/com/sheel/app/InsertOfferActivity.java
@@ -0,0 +1,23 @@
+package com.sheel.app;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * @author Mohsen
+ */
+public class InsertOfferActivity extends Activity {
+	
+	
+	 @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.insert_offer_1);
+	    }
+
+}
