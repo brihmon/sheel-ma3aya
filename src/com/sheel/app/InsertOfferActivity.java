01f82bd9297b14a5745fbda79960736e978f1d9a
amohsen1989 <amohsen1989@gmail.com>
2011-11-15 19:31:25 +0000

    Added the autosave(isch) feature to the InsertOfferActivity class

--- src/com/sheel/app/InsertOfferActivity.java
+++ src/com/sheel/app/InsertOfferActivity.java
@@ -4,23 +4,52 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+
+import com.sheel.datastructures.Offer;
 
 /**
  * @author Mohsen
  */
 public class InsertOfferActivity extends Activity {
+	static Offer offer ;// = new Offer(0,0,-1,"INVALID");
+    private EditText noKGsET;
+	private EditText pricePerKGET;
+	private RadioGroup type;
 	
 	 @Override
 	    public void onCreate(Bundle savedInstanceState) {
 	        super.onCreate(savedInstanceState);
 	        setContentView(R.layout.insert_offer_1);
-	    
+	        noKGsET = (EditText)findViewById(R.id.kgs);
+	        pricePerKGET = (EditText)findViewById(R.id.price);
+	        type = (RadioGroup)findViewById(R.id.typesOffers);
+	        
+	        if(offer!=null){
+	        	noKGsET.setText(offer.getNoOfKilograms()+"");
+	        	pricePerKGET.setText(offer.getPricePerKilogram()+"");
+	        	type.check(offer.getUserStatus()==0?R.id.avWeight:R.id.exWeight);
+	        }
+	        else{
+	        	type.check(R.id.exWeight);
+	        }
+	        
 	    }
 	 
 	 
 	 public void onClick_next(View v){
+		 
+		 
+		 
+		 offer = new Offer(
+				 !noKGsET.getText().toString().equals("")?
+				 Float.parseFloat(noKGsET.getText().toString()):0.0f,
+				 !pricePerKGET.getText().toString().equals("")?
+				 Float.parseFloat(pricePerKGET.getText().toString()):0.0f,
+				 type.getCheckedRadioButtonId()==R.id.exWeight?1:0,
+				 "new");
+		 
 		 startActivity(new Intent(this, InsertFlightActivity.class));
 		 
 	 }
