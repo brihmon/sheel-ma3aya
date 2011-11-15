76e3a9066933daf8b6b8e45283708747068156c6
amohsen1989@gmail.com <amohsen1989@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-11-15 18:58:39 +0000

    Committing client side Offer model

---
+++ trunk/src/com/sheel/datastructures/Offer.java
@@ -0,0 +1,85 @@
+package com.sheel.datastructures;
+
+
+
+public class Offer
+{
+
+	public Long id;
+	
+	/**
+	 * Flight ID for the offer (If available)
+	 */
+	public Long flightId;
+	
+	/**
+	 * User ID for the offer (If available)
+	 * Normally, this is the ID of the user using the application,
+	 * and it should not be needed since user-database interactions
+	 * are recorded on the server side.
+	 */
+	public Long userId;
+
+	public int pricePerKilogram;
+
+    public int noOfKilograms;
+
+    public int userStatus;  //1 for Extra weight and 0 for Less weight
+
+    public String offerStatus; 
+    
+    //public String currency; //Will be done next sprint
+    
+    public Offer(int noOfKilograms,
+            int pricePerKilogram,
+            int userStatus,
+            String offerStatus) 
+    {
+    	this.noOfKilograms = noOfKilograms;
+        this.pricePerKilogram = pricePerKilogram;
+        this.userStatus = userStatus;
+        this.offerStatus = offerStatus;
+
+    } 
+
+    public int getNoOfKilograms() {
+        return noOfKilograms;
+    }
+
+    public void setNoOfKilograms(int noOfKilograms) {
+        this.noOfKilograms = noOfKilograms;
+    }
+
+    public String getOfferStatus() {
+        return offerStatus;
+    }
+
+    public void setOfferStatus(String offerStatus) {
+        this.offerStatus = offerStatus;
+    }
+
+    public int getPricePerKilogram() {
+        return pricePerKilogram;
+    }
+
+    public void setPricePerKilogram(int pricePerKilogram) {
+        this.pricePerKilogram = pricePerKilogram;
+    }
+
+    public int getUserStatus() {
+        return userStatus;
+    }
+
+    public void setUserStatus(int userStatus) {
+        this.userStatus = userStatus;
+    }
+    
+
+	@Override
+	public String toString() {
+		return "Offer [id=" + id + ", noOfKilograms=" + noOfKilograms +
+				", pricePerKilogram=" + pricePerKilogram +
+				", userStatus=" + userStatus + ", offerStatus=" + offerStatus + "]";
+	}
+	
+}
