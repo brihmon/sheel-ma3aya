e6c56ffd1aa2d02770f896dcafb129ffabf11d3f
amohsen1989@gmail.com <amohsen1989@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-11-15 18:48:24 +0000

    Flight Clientside model

---
+++ trunk/src/com/sheel/datastructures/Flight.java
@@ -0,0 +1,88 @@
+package com.sheel.datastructures;
+
+import java.util.Date;
+ 
+/**
+ * @author Mohsen
+ * Under construction
+ * Client side model for flight
+ */
+public class Flight
+{
+	
+
+		public Long id;
+		
+	  	public String flightNumber;
+
+	    public String source;
+		
+	    public String destination;
+
+	    public String departureDateTime;  //Date only
+	    									 
+	//    @Filter("flight")
+	//    public Query <Offer> offers;
+	    
+	    public Flight(String flightNumber, String source, String destination, String departureDateTime) {
+	        this.flightNumber = flightNumber;
+	        this.source = source;
+	        this.destination = destination;
+	        this.departureDateTime = departureDateTime;
+	        
+	    }
+	    
+	    
+	    public String getDepartureDateTime() {
+	        return departureDateTime;
+	    }
+
+	    public void setDepartureDateTime(String departureDateTime) {
+	        this.departureDateTime = departureDateTime;
+	    }
+
+	    public String getDestination() {
+	        return destination;
+	    }
+
+	    public void setDestination(String destination) {
+	        this.destination = destination;
+	    }
+
+	    public String getFlightNumber() {
+	        return flightNumber;
+	    }
+
+	    public void setFlightNumber(String flightNumber) {
+	        this.flightNumber = flightNumber;
+	    }
+
+	    public String getSource() {
+	        return source;
+	    }
+
+	    public void setSource(String source) {
+	        this.source = source;
+	    }
+
+	    
+/*    
+	    public Query<Offer> getOffers() {
+	        return offers;
+	    }
+//
+	    public void setOffers(Query<Offer> offers) {
+	        this.offers = offers;
+	    }
+	    */
+
+
+		@Override
+		public String toString() {
+			return "Flight {id=" + id + ", flightNumber=" + flightNumber
+					+ ", source=" + source + ", destination=" + destination
+					+ ", departureDateTime=" + departureDateTime + "}";
+		}
+
+	    
+}
