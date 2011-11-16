cf22a44da2e4a9e42ec1d4cbc063f5d806d8e863
passant.elagroudy@gmail.com <passant.elagroudy@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-11-16 01:27:52 +0000

    ViewSearchResultsActivity: 1) Creation of appropriate adapter 2) linking of list view with that adapter 3) testing populating it by facebook users 4) creating a method to auto update the sliding drawer showing details 5) specifying to do tasks in the class to be completed later after discussing data structures with the team

--- trunk/src/com/sheel/app/ViewSearchResultsActivity.java
+++ trunk/src/com/sheel/app/ViewSearchResultsActivity.java
@@ -1,10 +1,22 @@
 
 package com.sheel.app;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.sheel.adapters.SearchResultsListAdapter;
+import com.sheel.datastructures.FacebookUser;
 
 /**
  * This activity is used for displaying and interacting with
@@ -14,6 +26,15 @@ import android.widget.TextView;
  *
  */
 public class ViewSearchResultsActivity extends Activity {
+	
+	/**
+	 * all owners of different offers
+	 */
+	ArrayList<FacebookUser> users;
+	/**
+	 * all offers to be displayed 
+	 */
+	ArrayList<Object> offers;
 
 	/** Called when the activity is first created. */
     @Override
@@ -21,6 +42,7 @@ public class ViewSearchResultsActivity extends Activity {
         super.onCreate(savedInstanceState);        
         setContentView(R.layout.search_result_details);
         setIconsForDetailsItems();
+        initListView();
     }// end onCreate
     
     /**
@@ -29,12 +51,7 @@ public class ViewSearchResultsActivity extends Activity {
      */
     private void setIconsForDetailsItems(){
     	    	
-    	/*Drawable img = getApplicationContext().getResources().getDrawable( R.drawable.details_money );
-    	img.setBounds( 0, 0, 60, 60 );
-    	TextView txtView = (TextView)findViewById(R.id.details_textView_price);
-    	txtView.setCompoundDrawables( img, null, null, null );*/
-    	
-    	setIconForATextField(R.id.details_textView_price, R.drawable.details_money,1);
+       	setIconForATextField(R.id.details_textView_price, R.drawable.details_money,1);
     	setIconForATextField(R.id.details_textView_facebookStatus, R.drawable.details_facebook,1);
     	setIconForATextField(R.id.details_textView_email, R.drawable.details_email,0);
     	setIconForATextField(R.id.details_textView_mobileNumber, R.drawable.details_phone,0);
@@ -51,7 +68,7 @@ public class ViewSearchResultsActivity extends Activity {
      * @param mode 
      * 		indicates size of icon.
      * 		<ul>
-     * 			<li>0: small    (30X30px)</li>
+     * 			<li>0: small    (40X40px)</li>
      * 			<li>1: medium   (50X50px)</li>
      * 			<li>2: large    (80X80px)</li>
      * 		</ul>
@@ -69,5 +86,108 @@ public class ViewSearchResultsActivity extends Activity {
     	TextView txtView = (TextView)findViewById(textViewId);
     	txtView.setCompoundDrawables( img, null, null, null );
     }// end SetIconForATextField
+    
+    /**
+     * used to initialize the values of the list view
+     */
+    private void initListView(){    	
+    
+    	// Get list using its ID
+    	ListView searchResultsList = (ListView)findViewById(R.id.listView_searchResults);
+    	
+    	new FacebookUser(-1, "firstName", "middleName", "lastName", true, "passant@gmail.com", true);
+    	users = new ArrayList<FacebookUser>();
+    	users.add(new FacebookUser(1,"Passant", "Akram", "El.Agroudy", true,"passant@gmail.com", true));
+    	users.add(new FacebookUser(2,"Hossam", "Mostafa", "Amer", false, "hossam@hotmail.com", true));
+    	users.add(new FacebookUser(3,"Nada", "", "Adly", true,"nada@yahoo.com", true));
+    	users.add(new FacebookUser(4,"Passant", "Akram", "El.Agroudy", true,"passant@gmail.com", false));
+    	users.add(new FacebookUser(5,"Hossam", "Mostafa", "Amer", false,"7as7as@gmail.com", true));
+    	users.add(new FacebookUser(6,"Nada", "", "Adly", true,"nido@msn.com", false));
+    	users.add(new FacebookUser(7,"Passant", "Akram", "El.Agroudy", true,"passant@gmail.com", true));
+    	//users.add(new FacebookUser(8,"Hossam", "Mostafa", "Amer", false));
+    	//users.add(new FacebookUser(9,"Nada", "", "Adly", true));
+    	 
+    	// Create adapter to control how list is displayed
+    	SearchResultsListAdapter adapter = new SearchResultsListAdapter(this, users);
+    	// Set adapter to the list view 	
+    	searchResultsList.setAdapter(adapter);
+    	    	
+    	// To enable filtering certain content of the method
+    	searchResultsList.setTextFilterEnabled(true);
+    	
+    	searchResultsList.setOnItemClickListener(new OnItemClickListener() {
+    	    public void onItemClick(AdapterView<?> parent, View view,
+    	        int position, long id) {
+    	     
+    	    	//Toast.makeText(getApplicationContext(), "Hello "+position, Toast.LENGTH_SHORT).show();
+    	    	
+    	    	// TODO simulate clicking of details button
+    	    	//Button detailsBtn = (Button)findViewById(R.id.button_slidingDrawer);
+    	    	    	    	
+    	    	// TODO : update details pane with appropriate data
+    	    	updateDetailsPane(position);
+    	    }// end onItemClick
+    	  });
+    
+    }// end initListView
+    
+    /**
+     * Method used to update content of sliding drawer displaying 
+     * different details about a certain offer
+     * 
+     * @param position 
+     * 		selected element in the list view. It is used
+     * 		to retrieve corresponding logical objects of this 
+     * 		element
+     * 
+     */
+    private void updateDetailsPane(int position){
+    	
+    	FacebookUser user = users.get(position);
+    	// TODO get offer
+    	
+    	// Get different GUI components to be updated
+    	TextView price = (TextView)findViewById(R.id.details_textView_price);
+    	TextView fbStatus = (TextView)findViewById(R.id.details_textView_facebookStatus);
+    	TextView mobile = (TextView)findViewById(R.id.details_textView_mobileNumber);
+    	TextView email = (TextView)findViewById(R.id.details_textView_email);
+    	
+    	// Set price value from offer
+    	// TODO currently static because i have no offer
+    	    	
+    	// get status from offer
+    	boolean wantToGive=true;
+    	String prefix="";
+    	
+    	// according to offer status -> specify prefix
+    	if (wantToGive){
+    		prefix = " Pays";
+    	}// end if : offer for giving luggage -> prefix: pay
+    	else{
+    		prefix = " Wants";
+    	}// end if : offer for carrying luggage -> prefix: wants
+    	
+    	// get price
+    	prefix += " 10 " + "dollar/Kg";
+    	
+    	// Set price
+    	price.setText(prefix);
+    	
+    	// Set facebook status of user 
+    	
+    	// TODO currently static because there is no common data structure
+    		
+    		// Set displayed text
+    		fbStatus.setText(" Facebook friend");
+    		// Set image for expansion
+    	
+    	// Set mobile from user in data base
+    	
+    	// TODO currently static because no common data structure
+    	mobile.setText("   +20105096872");
+    	
+    	// Set email from facebook user
+    	email.setText("   "+user.getEmail());
+    }// end updateDetailsPane
     
 }// end Activity
