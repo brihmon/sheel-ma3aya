1873f4e495cfe769fa9ef1ad158a3ca7b21a000c
amohsen1989 <amohsen1989@gmail.com>
2011-11-18 18:55:40 +0000

    Added HTTP POST  functionality

--- src/com/sheel/app/SheelMaaayaClient.java
+++ src/com/sheel/app/SheelMaaayaClient.java
@@ -1,15 +1,18 @@
 package com.sheel.app;
 
-import org.apache.http.HttpEntity;
+import java.net.URI;
+
 import org.apache.http.HttpResponse;
 import org.apache.http.HttpStatus;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 
-import android.os.Looper;
-import android.util.Log;
-import android.widget.Toast;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.sheel.datastructures.Flight;
 
 /**
  * 
@@ -23,6 +26,7 @@ import android.widget.Toast;
 public abstract class SheelMaaayaClient {
 	
 	 final String SERVER  = "http://sheelmaaayaa.appspot.com";
+	 //final String SERVER  = "http://192.168.1.3:9004";
      String PATH   = "";
      HttpResponse RESPONSE = null;
      Thread THREAD = null;
@@ -32,7 +36,71 @@ public abstract class SheelMaaayaClient {
 		
 	}
 	
+	 
+	// HttpResponse resposta = null;
+	//  StringEntity sEntity = new StringEntity(jsonString, "UTF-8");
+	//requisicao.setEntity(sEntity);
+
+//	resposta = httpClient.execute(requisicao);
+//	resultado = HttpProxy.leRespostaServidor(resposta);
 	
+	
+	static String JSON = "";
+	
+	/**
+	 * 
+	 * @param path is the path to the service
+	 * @param json : JSON object to be sent
+	 */
+	public void runHttpPost(String path,String json){
+	 
+	 
+		PATH = path;
+		JSON = json;
+	    rspStr = "";
+	    
+        THREAD = new Thread(new Runnable()
+    
+    	{
+        @Override
+        public void run()
+        {
+            try
+            {
+                DefaultHttpClient CLIENT = new DefaultHttpClient();   
+             	HttpPost httpost = new HttpPost();
+             	httpost.setURI(new URI(SERVER+PATH));
+            	httpost.setEntity(new StringEntity(JSON));
+            	
+             	httpost.setHeader("Accept", "text/javascript");
+        		httpost.setHeader("Content-type", "text/javascript");
+        		
+        		//httpost.setHeader("xx", JSON);
+        	//	Gson gson = new Gson();
+        	//	Flight x = gson.fromJson("{id: 10, flightNumber : LH123 }", Flight.class);
+        		
+                 RESPONSE = CLIENT.execute(httpost);
+                
+	          //   if (RESPONSE.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
+	           //     {
+	                 rspStr += EntityUtils.toString(RESPONSE.getEntity());
+	                 rspStr+=RESPONSE.getStatusLine().getStatusCode();
+	           //     }
+                
+            }
+            catch (Exception e)
+            {
+            	 rspStr = e.toString()+"- CLIENT ERROR";	
+            }
+            doSomething();
+        }
+    });
+    
+    THREAD.start();
+		JSON = "";
+		PATH = "";
+		
+	}
     
     public void runHttpRequest(String path){
 	   
@@ -48,6 +116,7 @@ public abstract class SheelMaaayaClient {
 	            try
 	            {
 	                 DefaultHttpClient CLIENT = new DefaultHttpClient();   
+	                 
 	                 RESPONSE = CLIENT.execute(new HttpGet(SERVER + PATH));
 	                
 		             if (RESPONSE.getStatusLine().getStatusCode() == HttpStatus.SC_OK)
@@ -58,7 +127,7 @@ public abstract class SheelMaaayaClient {
 	            }
 	            catch (Exception e)
 	            {
-	            	 rspStr = e.toString();	
+	            	 rspStr = e.toString()+"- CLIENT ERROR";	
 	            }
 	            doSomething();
 	        }
