3d2126dd8d8886c66c85507c472b6bbd6981f4b6
passant.elagroudy <passant.elagroudy@gmail.com>
2011-12-12 01:22:07 +0000

    1) Update type of expiry static variable to long instead of string 2) update method of setFacebookParameters where expiry is long instead of string 3) overload the method 4) override toString method 5) write a method to show content of the session

--- src/com/sheel/datastructures/Session.java
+++ src/com/sheel/datastructures/Session.java
@@ -15,7 +15,7 @@ package com.sheel.datastructures;
 public class Session {
 
 	/**
-	 * user ID in facebook to identify home page and other user-relevant
+	 * User ID in facebook to identify home page and other user-relevant
 	 * stuff
 	 */
 	public static String facebookUserId="";
@@ -24,9 +24,9 @@ public class Session {
 	 */
 	public static String facebookAccessToken = "";
 	/**
-	 * Expiry date of valid user access token
+	 * Remaining millisecs till the access token of facebook expires
 	 */
-	public static String facebookAccessTokenExpiry = "";
+	public static long facebookAccessTokenExpiry ;
 	
 	/**
 	 * Used to set all the parameters relevant to facebook in one step
@@ -36,16 +36,50 @@ public class Session {
 	 * @param accessToken
 	 * 		access token used in any query to the facebook API
 	 * @param expiry
-	 * 		expiry time of the access token (can be used to indicate)
-	 * 		whether user is logged in or not
+	 * 		number of millisecs remaining till the access token expires
+	 * 	    (can be used to indicate whether user is logged in or not)
 	 *
 	 * @author 
 	 *		Passant El.Agroudy (passant.elagroudy@gmail.com)
 	 */
-	public static void saveFaceookParameters(String userId, String accessToken, String expiry){
+	public static void saveFaceookParameters(String userId, String accessToken, long expiry){
 		facebookUserId = userId;
 		facebookAccessToken = accessToken;
 		facebookAccessTokenExpiry = expiry;				
-	}// end method: 
+	}// end saveFaceookParameters
 	
+	/**
+	 * Used to set all the parameters relevant to facebook retrieved
+	 * from the login process
+	 *
+	 * @param accessToken
+	 * 		access token used in any query to the facebook API
+	 * @param expiry
+	 * 		number of millisecs remaining till the access token expires
+	 * 	    (can be used to indicate whether user is logged in or not)
+	 *
+	 * @author 
+	 *		Passant El.Agroudy (passant.elagroudy@gmail.com)
+	 */
+	public static void saveFaceookParameters(String accessToken, long expiry){
+		saveFaceookParameters("", accessToken, expiry);
+	}// end saveFaceookParameters 
+	
+	/**
+	 * Represents information inside the session is readable way
+	 * 
+	 * @return 
+	 * 		string representing informating inside the session to be displayed
+	 *
+	 * @author 
+	 *		Passant El.Agroudy (passant.elagroudy@gmail.com)
+	 */
+	public static String showSessionInformation() {
+		return "Fb ID= " + facebookUserId + " , access token = "+ facebookAccessToken + " , initial expiry = "+ facebookAccessTokenExpiry;
+	}// end showSessionInformation
+	
+	@Override
+	public String toString() {
+		return "Fb ID= " + facebookUserId + " , access token = "+ facebookAccessToken + " , initial expiry = "+ facebookAccessTokenExpiry;
+	}// end toString
 }// end class
