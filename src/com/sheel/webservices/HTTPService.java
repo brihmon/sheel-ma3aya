8158a38576f204b2f99e52a5b17bd769a32a30ed
hossam.amer12 <hossam.amer12@gmail.com>
2011-12-14 17:23:28 +0000

    Giving a startup for the intentService to be used to handle HTTP requests.

---
+++ src/com/sheel/webservices/HTTPService.java
@@ -0,0 +1,25 @@
+package com.sheel.webservices;
+
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class HTTPService extends Service
+{
+	
+	
+	@Override
+	public IBinder onBind(Intent arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public void onHandleIntent(Intent intent)
+	{
+		
+		
+	}
+	
+}
