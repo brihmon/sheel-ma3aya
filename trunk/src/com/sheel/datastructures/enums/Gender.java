03e7662d7e77edeada0bf55963b2892bae1b33ac
passant.elagroudy@gmail.com <passant.elagroudy@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-12-14 19:17:12 +0000

    Create a new enumeration for Gender options for  consistency

---
+++ trunk/src/com/sheel/datastructures/enums/Gender.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.sheel.datastructures.enums;
+
+/**
+ * Enumeration representing the available options for
+ * gender
+ * 
+ * @author 
+ *		Passant El.Agroudy (passant.elagroudy@gmail.com)
+ *
+ */
+public enum Gender {
+	/**
+	 * Person is a female
+	 */
+	female,
+	/**
+	 * Person is a male
+	 */
+	male,
+	/**
+	 * The information is unavailable 
+	 */
+	unknown
+}// end enum
