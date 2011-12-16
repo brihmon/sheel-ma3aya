785e3cee4efe01c58d50dd6a80ed311b52d6d8b4
passant.elagroudy@gmail.com <passant.elagroudy@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-12-16 18:57:16 +0000

    Utils.java
    New class that has the common methods for manipulating data structures .. such as removing duplicates for example or sorting. 
    Currently the methods there are not used by the app. However they are uploaded in case someone needs them (were used before , before refactoring)

---
+++ trunk/src/com/sheel/datastructures/Utils.java
@@ -0,0 +1,71 @@
+/**
+ * 
+ */
+package com.sheel.datastructures;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.Iterator;
+
+/**
+ * Class used to group set of static methods used
+ * to manipulate different data structures such
+ * as removing duplicates and sorting
+ * 
+ * @author 
+ *		Passant El.Agroudy (passant.elagroudy@gmail.com)
+ *
+ */
+public final class Utils {
+	
+	private Utils() {
+		
+	}// end constructor
+	
+	/**
+	 * Used to remove all elements of <code>main</code> from <code>needsFiltering</code>
+	 * @param main
+	 * 		Reference hashtable that will be kept as is
+	 * @param needsFiltering
+	 * 		Hashtable that will get filtered (elements existing in main will be removed)
+	 * @author 
+	 * 		Passant El.Agroudy (passant.elagroudy@gmail.com)
+	 */
+	public static void removeDuplicates(Hashtable<String, OfferDisplay> main , Hashtable<String, OfferDisplay> needsFiltering){
+		
+		Iterator contentIt = main.keySet().iterator();
+		
+		while(contentIt.hasNext()){
+			String key = (String)contentIt.next();
+			
+			if (needsFiltering.containsKey(key)){
+				needsFiltering.remove(key);
+			}// end if : needsFiltering has duplicate from main -> remove
+			
+		}// end while: remove all elements in main from needsFiltering
+		
+	}// end removeDuplicates
+	
+	/**
+	 * Used to remove all elements of <code>main</code> from <code>needsFiltering</code>
+	 * <br><br><b>IMPORTANT: The method is generic and can be used with any objects
+	 * inside the arraylist. However, they must override {@link Object#equals(Object)}
+	 * method</b>
+	 * @param main
+	 * 		Reference arraylist that will be kept as is
+	 * @param needsFiltering
+	 * 		Reference that will get filtered (elements existing in main will be removed)
+	 * 		<br><b>IMPORTANT: The method will delete elements from <code>needsFiltering
+	 * 		</code></b>
+	 * @author 
+	 * 		Passant El.Agroudy (passant.elagroudy@gmail.com)
+	 */
+	public static void removeDuplicates(ArrayList<?> main, ArrayList<?> needsFiltering){
+		
+		if (needsFiltering.containsAll(main)) {
+			needsFiltering.removeAll(main);
+		}// end if: of elements in main is in needsFiltering -> remove
+	}// end removeDuplicates
+	
+
+}// end class
