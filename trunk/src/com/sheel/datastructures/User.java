d18f2722d63da41c546941c070d7372cc688f184
passant.elagroudy@gmail.com <passant.elagroudy@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-12-16 18:31:10 +0000

    User.Java 1) Added a new constructor for testing purposes 2) changed the access modifier of facebookAccount variable to private to avoid its setting any where in the app 3) created a getter for the facebookAccount field (getFacebookId)

--- trunk/src/com/sheel/datastructures/User.java
+++ trunk/src/com/sheel/datastructures/User.java
@@ -3,6 +3,8 @@ package com.sheel.datastructures;
 /**
  * 
  * @author Nada Emad
+ * @author 
+ * 		Passant El.Agroudy (passant.elagroudy@gmail.com)
  */
 
 
@@ -18,7 +20,7 @@ public class User
 	    public String passportNumber;
 	    public String email;
 	    public String mobileNumber;
-	    public String facebookAccount;
+	    private String facebookAccount;
 		public String gender;
 		public String nationality;
 	    
@@ -47,7 +49,34 @@ public class User
 
 	    }
 	    
+	    /**
+	     * Constructor created for testing purposes.
+	     * The constructor auto-sets the middle name to "middle" 
+	     * and last name to "last" for testing
+	     * 
+	     * @param facebookId
+	     * 		User Facebook ID
+	     * @param firstName
+	     * 		User First Name
+	     */
+	    public User (String facebookId, String firstName) {
+	    	this.facebookAccount = facebookId;
+	    	this.firstName = firstName;
+	    	this.middleName = "middle";
+	    	this.lastName = "last";
+	    		
+	    }// end constructor
 	    
+	    /**
+	     * Facebook ID of the user
+	     * @return
+	     * 		String representing the Facebook ID of the user
+	     * @author 
+	     *		Passant El.Agroudy (passant.elagroudy@gmail.com)
+	     */
+	    public String getFacebookId () {
+	    	return facebookAccount;
+	    }
 	    public String toString()
 	    {
 	    	return
