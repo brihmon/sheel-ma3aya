e28fd610313d520e9616d915fbb0792416b2bd40
hossam.amer12 <hossam.amer12@gmail.com>
2011-12-17 00:28:12 +0000

    Made a new class called InternetManager.. it checks for the availability of Internet connection
    I made it in a new class for everyone to be able to call it easily using the static method Nada helped me to do it.
    
    Still needs to be tested on a real situation of the app... this is just a first version for this class.

---
+++ src/com/sheel/utils/InternetManager.java
@@ -0,0 +1,54 @@
+package com.sheel.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.Log;
+
+/**
+ * InternetManager is a class that checks the availability of the Internet 
+ * @author Hossam_Amer
+ * @author Nada Adly
+ *
+ */
+
+public class InternetManager {
+
+	// Tag variable for logs & debugging
+	private static final String TAG = InternetManager.class.getName();
+	
+	/**
+	 * 
+	 * @param mContext The current context of the activity
+	 * @note You can get mContext by getApplicationContext or getContext, notsure!
+	 * @return
+	 * 	true Available Internet connection
+	 * 	false Not Available Internet connection
+	 * @author Hossam_Amer
+	 * @author Nada Adly
+	 */
+	
+	public static boolean isInternetOn(Context mContext) 
+	{
+		ConnectivityManager connec =  (ConnectivityManager) mContext.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+		// ARE WE CONNECTED TO THE NET
+		if ( connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTED ||
+		connec.getNetworkInfo(0).getState() == NetworkInfo.State.CONNECTING ||
+		connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTING ||
+		connec.getNetworkInfo(1).getState() == NetworkInfo.State.CONNECTED ) {
+		// MESSAGE TO SCREEN FOR TESTING (IF REQ)
+			//System.out.println("CONNECTED");
+		//Toast.makeText(this, connectionType + " connected", Toast.LENGTH_SHORT).show();
+			
+		Log.e(TAG, "Internet is Connected");	
+		return true;
+		} else if ( connec.getNetworkInfo(0).getState() == NetworkInfo.State.DISCONNECTED ||  connec.getNetworkInfo(1).getState() == NetworkInfo.State.DISCONNECTED  ) {
+		//System.out.println("Not Connected");
+			Log.e(TAG, "Internet is not Connected");
+		return false;
+		}
+		Log.e(TAG, "Internet is not Connected");
+		return false;
+	}
+}
