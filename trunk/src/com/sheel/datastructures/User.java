4aa74fba5eff869fd0d1182424fdc1d282868c5f
nada.adly <nada.adly@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-12-20 18:34:39 +0000

    Upload photo to server successfully. added a method in starthttprequest to post json objects. Forgot to commit the changes i made in User datastructure

--- trunk/src/com/sheel/datastructures/User.java
+++ trunk/src/com/sheel/datastructures/User.java
@@ -12,7 +12,7 @@ public class User
 {
 		
 		public Long id;
-
+		public String username;
 	    public String firstName;
 	    public String middleName;
 	    public String lastName;
@@ -36,6 +36,33 @@ public class User
 	            String gender,
 	            String nationality) {
 	     
+	        this.firstName = firstName;
+	        this.middleName = middleName;
+	        this.lastName = lastName;
+	        this.passportPhoto = passportPhoto;
+	        this.passportNumber = passportNumber;
+	        this.email = email;
+	        this.mobileNumber = mobileNumber;
+	        this.facebookAccount = facebookAccount;
+	        this.gender = gender;
+	        this.nationality = nationality;
+
+	    }
+	    
+	    //nada
+	    public User(
+	    		String username, 
+	            String firstName,
+	            String middleName,
+	            String lastName,
+	            String passportPhoto,
+	            String passportNumber,
+	            String email,
+	            String mobileNumber,
+	            String facebookAccount,
+	            String gender,
+	            String nationality) {
+	        this.username = username;
 	        this.firstName = firstName;
 	        this.middleName = middleName;
 	        this.lastName = lastName;
