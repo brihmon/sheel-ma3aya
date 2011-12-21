ad6078bd4e04a32402f782228e7ef9c25bf23c33
hossam.amer12@gmail.com <hossam.amer12@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-12-21 13:08:56 +0000

    Fixed the return type of my controller to be easily handled by the Android Client

--- trunk/src/com/sheel/datastructures/Confirmation.java
+++ trunk/src/com/sheel/datastructures/Confirmation.java
@@ -1,5 +1,7 @@
 package com.sheel.datastructures;
 
+import org.json.JSONObject;
+
 public class Confirmation {
 
 	
@@ -13,5 +15,59 @@ public class Confirmation {
 	public final static String confirmedByAnotherPerson = "confirmedByAnotherPerson";
 	public final static String half_confirmed = "half-confirmed";
 	public final static String not_confirmed = "new";
+	
+	
+	/**
+	 * Used for mapping the confirmation from JSONObject into a normal confirmation
+	 * 
+	 * @param
+	 * 		offerJSON JSON Object sent to be converted to a normal offer
+	 * @return OfferDisplay2 Object from the JSONObject
+	 * @author Hossam_Amer
+	 */
+	
+	public static OfferDisplay2 mapConfirmation(JSONObject confirmationJSON)
+	{
+		try {
+			
+			JSONObject userJSON = confirmationJSON.getJSONObject("user");
+			JSONObject flightJSON = confirmationJSON.getJSONObject("flight");
+			
+			String ownerId = userJSON.getString("facebookAccount");
+			String firstName = userJSON.getString("firstName");
+			String middleName = userJSON.getString("middleName");
+			String lastName = userJSON.getString("lastName");
+			String email = userJSON.getString("email");
+			String mobile = userJSON.getString("mobileNumber");
+			String gender = userJSON.getString("gender");
+			String nationality = userJSON.getString("nationality");
+			
+			User user = new User(ownerId, firstName, middleName, lastName, "", "", email, mobile, gender, nationality);
+			
+			Long offerId = confirmationJSON.getLong("id");
+			String offerstatus = confirmationJSON.getString("offerStatus");
+			int userstatus = confirmationJSON.getInt("userStatus");
+			int kgs = confirmationJSON.getInt("noOfKilograms");
+			int price = confirmationJSON.getInt("pricePerKilogram");
+			
+			Offer offer = new Offer(offerId, kgs, price, userstatus, offerstatus);
+			    
+			String flightNumber = flightJSON.getString("flightNumber");
+			String source = flightJSON.getString("source");
+			String destination = flightJSON.getString("destination");
+			String departureDate = flightJSON.getString("departureDate");
+				
+			Flight flight = new Flight(flightNumber, source, destination, departureDate);
+			
+				
+			return new OfferDisplay2(user, flight, offer);
+
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			
+			return null;
+		}
+	}
 
 }
