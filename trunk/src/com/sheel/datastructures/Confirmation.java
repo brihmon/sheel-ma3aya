409d3a3c763eadd74a42351aff634c95e4ac377f
hossam.amer12@gmail.com <hossam.amer12@gmail.com@74e3d63a-fc87-3f28-2a58-97915b43a7cd>
2011-12-21 13:01:44 +0000

    Confirm offer is in progress

---
+++ trunk/src/com/sheel/datastructures/Confirmation.java
@@ -0,0 +1,17 @@
+package com.sheel.datastructures;
+
+public class Confirmation {
+
+	
+	/**
+	 * Confirmation Statuses
+	 */
+	
+	public final static String alreadyConfirmed = "alreadyConfirmed";
+	public final static String notFromOfferOwner = "notFromOfferOwner";
+	public final static String notSameUser = "notSameUser";
+	public final static String confirmedByAnotherPerson = "confirmedByAnotherPerson";
+	public final static String half_confirmed = "half-confirmed";
+	public final static String not_confirmed = "new";
+
+}
